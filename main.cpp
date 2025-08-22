3ecdc51f8ec8eab315ed06d735b27d7b36578017
alexLara97 <alex.lara.cont97@gmail.com>
2025-08-22 12:28:06 +0200

    Addition of CPU usage function and main implementation

--- main.cpp
+++ main.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 struct CPUData {
-    unsigned long long user, nice, system, idle, iowait, irq, softirp, steal, guest, guest_nice;
+    unsigned long long user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice;
 };
 
-CPUData readCPUStats() {
+CPUData readCPUStatus() {
     ifstream file("/proc/stat");
     string line;
     CPUData data{};
     
     if (file.is_open()) {
-        getline(file, line)
-        
+        getline(file, line);
+        istringstream iss(line);
+        string cpu;
+        iss >> cpu >> data.user  >> data.nice >> data.system >> data.idle >> data.iowait 
+            >> data.irq >> data.softirq >> data.steal >> data.guest >> data.guest_nice;
     }
-    
+    return data;
+}
+
+float calculateCPUUsage(const CPUData& prev, const CPUData& curr){
+    unsigned long long prevIdle = prev.idle + prev.iowait;
+    unsigned long long idle = curr.idle + curr.iowait;
+
+    unsigned long long preNonIdle = prev.user + prev.nice + prev.system + prev.irq + prev.softirq + prev.steal;
+    unsigned long long nonIdle = curr.user + curr.nice + curr.system + curr.irq + curr.softirq + curr.steal;
+    unsigned long long prevTotal = prevIdle + preNonIdle;
+    unsigned long long total = idle + nonIdle;
+
+    unsigned long long totalDiff = total - prevTotal;
+    unsigned long long idleDiff = idle - prevIdle;
+
+    return (float)(totalDiff - idleDiff) / totalDiff * 100.0;
+}
+
+int main() {
+    CPUData prev = readCPUStatus();
+    this_thread::sleep_for(chrono::seconds(1));
+    CPUData curr = readCPUStatus();
+
+    float cpuUsage = calculateCPUUsage(prev, curr);
+    cout<<"CPU Usage: " << cpuUsage << "%" << endl;
+
+    return 0;
 }
